da54775dcb6586b6be492a1f81c7c580c6abb1b8
Jiajun Huang <gansteed@gmail.com>
2019-06-18 15:45:30 +0800

    add basic templates

--- config.py
+++ config.py
@@ -6,6 +6,15 @@ class Config:
         self.SQLALCHEMY_DB_URI = os.getenv("SQLALCHEMY_DB_URI", "sqlite:////data/db/tools.db")
         self.SQLALCHEMY_ECHO = os.getenv("SQLALCHEMY_ECHO") == "True"
         self.DEBUG = os.getenv("DEBUG") == "True"
+        self.SENTRY_DSN = os.getenv("SENTRY_DSN", "")
+
+        # 配置，一般是全局变量
+        self.FLASK_CONFIG = {
+            "SITE_NAME": os.getenv("SITE_NAME", "网站标题"),
+            "SITE_SLOGAN": os.getenv("SITE_SLOGAN", "网站slogan"),
+            "GA_CODE": os.getenv("GA_CODE"),  # Google Analytics Code
+            "DISABLE_AD": os.getenv("DISABLE_AD") == "True",
+        }
 
 
 config = Config()
--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 import logging
 
 from flask import Flask
+import sentry_sdk
+from sentry_sdk.integrations.flask import FlaskIntegration
 
 from loadbp import load_bp
+from config import config
 
 logging.basicConfig(level=logging.INFO)
 
 app = Flask(__name__)
 load_bp(app)
+# global variables
+for k, v in config.FLASK_CONFIG.items():
+    app.config[k] = v
+
+if config.SENTRY_DSN:
+    logging.info("integrating sentry...")
+    sentry_sdk.init(
+        dsn=config.SENTRY_DSN,
+        integrations=[FlaskIntegration()]
+    )
 
 
 @app.route("/ping")
